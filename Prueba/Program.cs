d48dcdd07b1ce76139c8118bd40a0677a2e999ec
Jorge <jorgemaga21@hotmail.es>
2024-12-06 23:14:31 +0100

    Entrada nombre y edad

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,8 +1,8 @@
-﻿string nombre = "Jorge";
-Console.WriteLine($"Hola {nombre}, buenos días.");
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine("Modificado desde GitHub");
-Console.ReadKey();
+﻿string nombre;
+int edad;
+Console.Write("Hola, Introduce tu nombre: ");
+nombre = Console.ReadLine();
+Console.Write("Introduce tu edad: ");
+edad = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine($"Te llamas {nombre} y tienes {edad} años.");
+
